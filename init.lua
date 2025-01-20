869604c59081591e53edf84b6a868cdf7c5ee213
vin3y <vinaysankar7@gmail.com>
2025-01-20 20:12:58 +0530

    Edited plugins to folder

--- init.lua
+++ init.lua
@@ -12,31 +12,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-   },
-   {
-       'shaunsingh/nord.nvim',
-       lazy=false,
-       priority = 1000,
-       config = function()
-           -- Example config in lua
-vim.g.nord_contrast = true
-vim.g.nord_borders = false
-vim.g.nord_disable_background = false
-vim.g.nord_italic = false
-vim.g.nord_uniform_diff_background = true
-vim.g.nord_bold = false
-
--- Load the colorscheme
-require('nord').set()
-       end
-   }
+    require 'plugins.neotree',
+  require 'plugins.colortheme',
 })

87de9d56ff2ab7113171bfbe78b4c553ac4ad819
vin3y <vinaysankar7@gmail.com>
2025-01-20 17:06:22 +0530

    Added modified neotree

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+require 'core.options'
+require 'core.keymaps'
+
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -16,6 +20,23 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-}
+    },
+   },
+   {
+       'shaunsingh/nord.nvim',
+       lazy=false,
+       priority = 1000,
+       config = function()
+           -- Example config in lua
+vim.g.nord_contrast = true
+vim.g.nord_borders = false
+vim.g.nord_disable_background = false
+vim.g.nord_italic = false
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = false
+
+-- Load the colorscheme
+require('nord').set()
+       end
+   }
 })
